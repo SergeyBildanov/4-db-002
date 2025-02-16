f98c5c551e0f8c488dd46508e465cec363c04777
SergeyBildanov <troktrokov@gmail.com>
2025-02-17 00:34:54 +0400

    database and index.php changes

--- index.php
+++ index.php
@@ -3,118 +3,88 @@ require_once "autoloader.php";
 spl_autoload_register('autoloader');
 
 $pdo = new PDO("sqlite:./database.db");
-/*
-$sth = $pdo->query("SELECT * from shop");
-$rows = $sth->fetchAll();
-foreach ($rows as $row){
-    echo $row['shopID'] . "|".$row['name']."|".$row['address'].PHP_EOL;
-}
 
-$sth = $pdo->query("SELECT * from product");
-$rows = $sth->fetchAll();
-foreach ($rows as $row){
-    echo $row['productID'] . "|".$row['productName']."|".$row['productPrice']."|".$row['productCount'].PHP_EOL;
-}
+$pdo->query("CREATE TABLE IF NOT EXISTS shop (
+    shopID INTEGER,
+    'name' VARCHAR,
+    address VARCHAR,
+    PRIMARY KEY (shopID AUTOINCREMENT)
+)");
 
-$sth = $pdo->query("SELECT * from client");
-$rows = $sth->fetchAll();
-foreach ($rows as $row){
-    echo $row['clientID'] . "|".$row['clientName']."|".$row['phone']."|".$row['birthday'].PHP_EOL;
-}
+$pdo->query("CREATE TABLE IF NOT EXISTS product (
+    productID INTEGER,
+    productName VARCHAR,
+    productPrice INT,
+    productCount INT,
+    PRIMARY KEY (productID AUTOINCREMENT)
+)");
 
-$sth = $pdo->query("SELECT * from 'order'");
-$rows = $sth->fetchAll();
-foreach ($rows as $row){
-    echo $row['orderID'] . $row['created_at'] . "|".$row['shopID']."|".$row['clientID'].PHP_EOL;
-}
+$pdo->query("CREATE TABLE IF NOT EXISTS client (
+    clientID INTEGER,
+    clientName VARCHAR,
+    phone VARCHAR,
+    birthday DATE,
+    PRIMARY KEY (clientID AUTOINCREMENT)
+)");
 
-$sth = $pdo->query("SELECT * from order_product");
-$rows = $sth->fetchAll();
-foreach ($rows as $row){
-    echo $row['id'] . $row['orderID'] . "|".$row['productID']."|".$row['price'].PHP_EOL;
-}
-*/
-$sth = $pdo->query("SELECT * from 'order_product'");
-$rows = $sth->fetchAll();
-foreach ($rows as $row){
-    echo $row['id'] . "|".  $row['orderID'] . "|".$row['productID']."|".$row['price'].PHP_EOL;
-}
-$sth = $pdo->query("SELECT clientID, clientName, birthday from client");
-$clients = $sth->fetchAll();
-foreach ($clients as $client){
-    echo $client['clientID'] . "|".  $client['clientName'] .  "|".  $client['birthday'] .PHP_EOL;
-}
-$sth = $pdo->query("SELECT * from 'order'");
-$orders = $sth->fetchAll();
-foreach ($orders as $order){
-    echo $order['orderID'] . "|".  $order['shopID'] . "|".$order['clientID']."|". $order['created_at'].PHP_EOL;
-}
+$pdo->query("CREATE TABLE IF NOT EXISTS 'order' (
+    orderID INTEGER,
+    shopID INT,
+    clientID INT,
+    created_at DATE,
+    FOREIGN KEY (shopID) REFERENCES shop (shopID),
+    FOREIGN KEY (clientID) REFERENCES client (clientID),
+    PRIMARY KEY (orderID AUTOINCREMENT)
+)");
 
-foreach ($clients as $client){
-    $query = "SELECT * from 'order' WHERE clientID=" . $client['clientID'];
-    $rows = $pdo->query($query)->fetchAll();
-    $result = 0;
-    foreach ($rows as $row){
-        $orderDate = new DateTime($row['created_at']);
-        $birthDate = new DateTime($client['birthday']);
-        //echo $orderDate->format('d.m.Y') . "|" . $birthDate->format('d.m.Y') . PHP_EOL;
-        $interval = $birthDate->diff($birthDate);
-        $sum = 0;
-        if((int)$orderDate->format('Y') > (int)$birthDate->format('Y') and $interval->d <= 3){
-            $newQuery = "SELECT price FROM order_product WHERE orderID=" . $row['orderID'];
-            $sth = $pdo->query($newQuery);
-            $prices = $sth->fetchAll();
-            foreach ($prices as $price){
-                $sum += $price['price'];
-            }
-        }
-        $result += $sum;
-    }
-    echo $client['clientName'] . "|" .  $result. PHP_EOL;
+$pdo->query("CREATE TABLE IF NOT EXISTS 'order_product' (
+    id INTEGER,
+    orderID INT,
+    productID INT,
+    price INT,
+    FOREIGN KEY (orderID) REFERENCES 'order' (orderID),
+    FOREIGN KEY (productID) REFERENCES product (productID),
+    PRIMARY KEY (id AUTOINCREMENT)
+)");
+
+$sth = $pdo->query("SELECT SUM(price) as order_price, client.clientName FROM 'order_product' 
+         JOIN 'order' ON order_product.orderID='order'.orderID
+         JOIN client ON client.clientID='order'.clientID
+		 WHERE ABS(strftime('%d', created_at)-strftime('%d', birthday)) < 3
+         GROUP BY (client.clientName) ");
+$rows = $sth->fetchAll();
+foreach ($rows as $row){
+    echo $row["clientName"] . "|".  $row['order_price'] . PHP_EOL;
 }
 echo "----------------------------------------------------------------------------------------------------------" . PHP_EOL;
-$sth = $pdo->query("SELECT * from 'order_product'");
-$order_products = $sth->fetchAll();
-foreach ($order_products as $order_product){
-    $query = "SELECT * from product WHERE productID= " . $order_product['productID'];
-    $sth2 = $pdo->query($query);
-    $prices = $sth2->fetchAll();
-    foreach ($prices as $price){
-        if((int)$order_product['price']-(int)$price['productPrice'] !== 0){
-            echo "Заказ " . $order_product['id'] . "|".  $order_product['orderID'] . "|".$order_product['productID']."|".$order_product['price']. "|" ."Разница: " . abs((int)$order_product['price']-(int)$price['productPrice']) . PHP_EOL;
-        }
-    }
+$sth = $pdo->query("SELECT order_product.price - product.productPrice as difference, order_product.orderID FROM 'order_product' 
+         JOIN product ON order_product.productID=product.productID
+		 WHERE order_product.price <> product.productPrice
+         GROUP BY (order_product.orderID) ");
+$rows = $sth->fetchAll();
+foreach ($rows as $row){
+    echo  "Заказ " . $row["orderID"] . "|".  $row['difference'] . PHP_EOL;
 }
 echo "----------------------------------------------------------------------------------------------------------" . PHP_EOL;
-$incomes = [];
-$sells = [];
-$sth = $pdo->query("SELECT * from 'order'");
-$orders = $sth->fetchAll();
-foreach ($orders as $order){
-    $query = "SELECT * from 'order_product' where orderID=" . $order['orderID'];
-    //echo $query . PHP_EOL;
-    $sth = $pdo->query($query);
-    $order_products = $sth->fetchAll();
-    //echo "Магазин " . $order["shopID"] . PHP_EOL;
-    if(!array_key_exists( $order["shopID"] ,$incomes)){
-        $incomes[$order["shopID"]] = 0;
-    }
-    if(!array_key_exists( $order["shopID"] ,$sells)){
-        $sells[$order["shopID"]] = 0;
-    }
-    foreach ($order_products as $order_product){
-        $incomes[$order["shopID"]] += $order_product["price"];
-        $sells[$order["shopID"]]+=1;
-    }
+$sth = $pdo->query("SELECT SUM(price) as order_price, 'order'.shopID, shop.name FROM 'order_product' 
+         JOIN 'order' ON order_product.orderID='order'.orderID
+		 JOIN shop ON shop.shopID='order'.shopID
+         GROUP BY 'order'.shopID
+		 ORDER BY order_price DESC
+		 limit 1; ");
+$rows = $sth->fetchAll();
+echo "Самый прибыльный магазин:";
+foreach ($rows as $row){
+    echo $row["name"] . "|".  $row['order_price'] . PHP_EOL;
 }
-$query1 = "SELECT name from shop where shopID=" . array_search(max($incomes),$incomes);
-$query2 = "SELECT name from shop where shopID=" . array_search(max($sells),$sells);
-
-$stmt = $pdo->query($query1);
-$smth = $stmt->fetchAll();
-$mostIncomeShop = $smth[0]['name'];
-$stmt = $pdo->query($query2);
-$smth = $stmt->fetchAll();
-$mostPopularShop = $smth[0]['name'];
-echo "Самый прибыльный магазин: $mostIncomeShop" . PHP_EOL;
-echo "Самый большое количество заказов у магазина $mostPopularShop". PHP_EOL;
+$sth = $pdo->query("SELECT COUNT('order'.orderID) as orders, 'order'.shopID, shop.name FROM 'order_product' 
+         JOIN 'order' ON order_product.orderID='order'.orderID
+		 JOIN shop ON shop.shopID='order'.shopID
+         GROUP BY ('order'.shopID)
+		 ORDER BY orders DESC
+		 limit 1; ");
+$rows = $sth->fetchAll();
+echo "Наибольшее число покупок в магазине:";
+foreach ($rows as $row){
+    echo $row["name"] . "|".  $row['orders'] . PHP_EOL;
+}
